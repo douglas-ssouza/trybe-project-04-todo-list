8a3f0b20c5d4c41959c494331bdee1ec45782147
Douglas Souza <douglassouzabjj@hotmail.com>
2021-07-09 16:26:14 -0300

    Adiciona requisitos bonus

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const input = document.getElementById('texto-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const btnAdd = document.getElementById('criar-tarefa');
 const btnClear = document.getElementById('apaga-tudo');
-const btnRemove = document.getElementById('remover-finalizados');
+const btnRemCompleted = document.getElementById('remover-finalizados');
+const btnSave = document.getElementById('salvar-tarefas');
+const btnUp = document.getElementById('mover-cima');
+const btnDown = document.getElementById('mover-baixo');
+const btnRemSelected = document.getElementById('remover-selecionado');
 
 function addTask() {
   const texto = input.value;
@@ -35,6 +39,25 @@ function isCompleted(classes) {
   return false;
 }
 
+function isSelected(classes) {
+  for (const classe of classes) {
+    if (classe === 'selected'){
+      return true;
+    }
+  }
+  return false;
+}
+
+function removeSeleceted() {
+  for (let index = lista.children.length - 1; index >= 0; index -= 1) {
+    if (isSelected(lista.children[index].classList)) {
+      lista.removeChild(lista.children[index]);
+      break;
+    }
+  }
+}
+btnRemSelected.addEventListener('click', removeSeleceted);
+
 function completeTask(event) {
   completed = isCompleted(event.target.classList);
 
@@ -46,12 +69,6 @@ function completeTask(event) {
 }
 lista.addEventListener('dblclick', completeTask)
 
-function clearList() {
-  for (let index = lista.children.length - 1; index >= 0; index -= 1) {
-    lista.removeChild(lista.children[index]);
-  }
-}
-btnClear.addEventListener('click', clearList);
 
 function removeCompleted() {
   for (let index = lista.children.length - 1; index >= 0; index -= 1) {
@@ -60,4 +77,20 @@ function removeCompleted() {
     }
   }
 }
-btnRemove.addEventListener('click', removeCompleted);
+btnRemCompleted.addEventListener('click', removeCompleted);
+
+function clearList() {
+  for (let index = lista.children.length - 1; index >= 0; index -= 1) {
+    lista.removeChild(lista.children[index]);
+  }
+}
+btnClear.addEventListener('click', clearList);
+
+function saveTasks() {
+  localStorage.setItem('items', lista.innerHTML);
+}
+btnSave.addEventListener('click', saveTasks);
+
+window.onload = function () {
+  lista.innerHTML = localStorage.getItem('items');
+};

27c339d575db76b7e14e27fff6347c010eca72f3
Douglas Souza <douglassouzabjj@hotmail.com>
2021-07-09 15:08:43 -0300

    Adiciona requisitos 1 a 9

---
+++ script.js
@@ -0,0 +1,54 @@
+const input = document.getElementById('texto-tarefa');
+const lista = document.getElementById('lista-tarefas');
+const btnAdd = document.getElementById('criar-tarefa');
+const btnClear = document.getElementById('apaga-tudo');
+
+function addTask() {
+  const texto = input.value;
+  const itemLista = document.createElement('li');
+  itemLista.innerText = texto;
+  lista.appendChild(itemLista);
+  input.value = '';
+  input.blur();
+}
+btnAdd.addEventListener('click', addTask);
+
+function removeSelected() {
+  for (const item of lista.children) {
+    item.classList.remove('selected');
+  }
+}
+
+function selectItem(event) {
+  removeSelected()
+  event.target.classList.add('selected');
+}
+lista.addEventListener('click', selectItem);
+
+function isCompleted(event) {
+  completed = false;
+  for (const classe of event.target.classList) {
+    if (classe === 'completed') {
+      return true;
+    }
+  }
+  return false;
+}
+
+function completeTask(event) {
+  completed = isCompleted(event);
+
+  if (completed) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed')
+  }
+}
+lista.addEventListener('dblclick', completeTask)
+
+function clearList() {
+  for (const index = lista.children.length - 1; index >= 0; index -= 1) {
+    lista.removeChild(lista.children[index]);
+  }
+}
+btnClear.addEventListener('click', clearList);
